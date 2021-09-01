2f5f7ba4e18f9663132b9b1796a39fe4bb06f1f0
jdavidtorres <jesu_ti91@hotmail.com>
2021-09-01 17:46:35 -0500

    dev

--- src/main/java/co/com/jdti/school/SchoolApplication.java
+++ src/main/java/co/com/jdti/school/SchoolApplication.java
@@ -1,15 +1,60 @@
 package co.com.jdti.school;
 
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import co.com.jdti.school.model.entities.Course;
+import co.com.jdti.school.model.entities.Student;
+import co.com.jdti.school.model.entities.Subject;
+import co.com.jdti.school.model.entities.Teacher;
+import co.com.jdti.school.services.ICourseServices;
+import co.com.jdti.school.services.IStudentServices;
+import co.com.jdti.school.services.ISubjectServices;
+import co.com.jdti.school.services.ITeacherServices;
+
 @SpringBootApplication
 public class SchoolApplication implements CommandLineRunner {
 
+	@Autowired
+	private IStudentServices iStudentServices;
+
+	@Autowired
+	private ICourseServices iCourseServices;
+
+	@Autowired
+	private ITeacherServices iTeacherServices;
+
+	@Autowired
+	private ISubjectServices iSubjectServices;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Course course1 = iCourseServices
+				.save(new Course("aka1", "2A", iTeacherServices.save(new Teacher("id1", "Laracrisa Euclidiana")),
+						new ArrayList<>(), new ArrayList<>()));
+		iCourseServices.save(new Course("aka1", "3A", iTeacherServices.save(new Teacher("id1", "Pepito Perez")),
+				new ArrayList<>(), new ArrayList<>()));
+		iCourseServices.save(new Course("aka2", "4A", iTeacherServices.save(new Teacher("id2", "Fulanito de Tal")),
+				new ArrayList<>(), new ArrayList<>()));
+		iCourseServices.save(new Course("aka3", "5A", iTeacherServices.save(new Teacher("id3", "Juanita Perez")),
+				new ArrayList<>(), new ArrayList<>()));
+		iCourseServices.save(new Course("aka4", "6A", iTeacherServices.save(new Teacher("id4", "Pepito Jr. Perez")),
+				new ArrayList<>(), new ArrayList<>()));
+		iStudentServices.save(new Student("aka1", "dni1", "Hugo", "Fulano", "A-", null, null, "Calle 1", course1));
+		iStudentServices
+				.save(new Student("aka2", "dni2", "Paco", "Perez", "O-", "Pepito", "Perez", "Calle 2", course1));
+		iStudentServices.save(new Student("aka3", "dni3", "Luis", "Mengano", "O+", "", "", "Calle 3", null));
+		iStudentServices.save(new Student("aka4", "dni4", "Paco", "Mengano", "O+", "", "", "Calle 4", null));
+		iSubjectServices.save(new Subject(1, "Matemáticas", null));
+		iSubjectServices.save(new Subject(2, "Lenguaje", null));
+		iSubjectServices.save(new Subject(3, "Historia", null));
 	}
 }
