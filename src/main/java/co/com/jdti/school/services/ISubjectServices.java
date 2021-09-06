163b3b2d95d53937098d40de4065e603da57ac4e
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-06 17:25:54 -0500

    Update ISubjectServices.java

--- src/main/java/co/com/jdti/school/services/ISubjectServices.java
+++ src/main/java/co/com/jdti/school/services/ISubjectServices.java
@@ -1,12 +1,15 @@
 package co.com.jdti.school.services;
 
-import java.util.List;
-
 import co.com.jdti.school.model.entities.Subject;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ISubjectServices {
 
 	List<Subject> findAll();
 
 	Subject save(Subject subject);
+
+	Optional<Subject> findById(Long id);
 }
