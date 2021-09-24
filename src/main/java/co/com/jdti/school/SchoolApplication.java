f17be072d5a56d5600ca804294d6b736fd8731ef
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 15:55:55 -0500

    Update SchoolApplication.java

--- src/main/java/co/com/jdti/school/SchoolApplication.java
+++ src/main/java/co/com/jdti/school/SchoolApplication.java
@@ -31,30 +31,6 @@ public class SchoolApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Teacher teacher2A = iTeacherServices.save(Teacher.builder().name("Laracrisa Euclidiana").build());
-        Teacher teacher3A = iTeacherServices.save(Teacher.builder().name("Pepito Perez").build());
-
-        Course course1 = iCourseServices.save(Course.builder()
-                .name("2A")
-                .director(teacher2A)
-                .build());
-        iCourseServices.save(Course.builder()
-                .name("3A")
-                .director(teacher3A)
-                .build());
-        iCourseServices.save(new Course("aka2", "4A", iTeacherServices.save(new Teacher("id2", "Fulanito de Tal")), null));
-        iCourseServices.save(new Course("aka3", "5A", iTeacherServices.save(new Teacher("id3", "Juanita Perez")), new ArrayList<>()));
-        iCourseServices.save(new Course("aka4", "6A", iTeacherServices.save(new Teacher("id4", "Pepito Jr. Perez")), new ArrayList<>()));
-        iStudentServices.save(new Student("aka1", "dni1", "Hugo", "Fulano", "A-", null, null, "Calle 1", course1));
-        iStudentServices.save(new Student("aka2", "dni2", "Paco", "Perez", "O-", "Pepito", "Perez", "Calle 2", course1));
-        iStudentServices.save(new Student("aka3", "dni3", "Luis", "Mengano", "O+", "", "", "Calle 3", null));
-        iStudentServices.save(new Student("aka4", "dni4", "Paco", "Mengano", "O+", "", "", "Calle 4", null));
-        iSubjectServices.save(Subject.builder().name("Matemáticas").build());
-        iSubjectServices.save(Subject.builder().name("Lenguaje").build());
-        iSubjectServices.save(Subject.builder().name("Historia").build());
-        iSubjectServices.save(Subject.builder().name("Geometría").build());
-        iSubjectServices.save(Subject.builder().name("Ética").build());
-        iSubjectServices.save(Subject.builder().name("Matemáticas").build());
         Teacher teacher2A = teacherServices.save(Teacher.builder().name("Laracrisa Euclidiana").build());
         Teacher teacher3A = teacherServices.save(Teacher.builder().name("Pepito Perez").build());
 
