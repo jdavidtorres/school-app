50840c80b1b16b9df0bf4dc7349d998d04dceb71
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 15:52:27 -0500

    Update IndexController.java

--- src/main/java/co/com/jdti/school/controllers/IndexController.java
+++ src/main/java/co/com/jdti/school/controllers/IndexController.java
@@ -5,7 +5,7 @@ import co.com.jdti.school.model.entities.Student;
 import co.com.jdti.school.services.CoursesServices;
 import co.com.jdti.school.services.StudentServices;
 import co.com.jdti.school.services.SubjectServices;
-import co.com.jdti.school.services.TeacherServicesImpl;
+import co.com.jdti.school.services.TeacherServices;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -23,117 +23,110 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class IndexController {
 
-	@Autowired
-	private IStudentServices iStudentServices;
+    private final StudentServices studentServices;
+    private final CoursesServices coursesServices;
+    private final TeacherServices teacherServices;
+    private final SubjectServices subjectServices;
 
-	@Autowired
-	private ICourseServices iCourseServices;
+    private List<Student> getStudents() {
+        return studentServices.findAll();
+    }
 
-	@Autowired
-	private ITeacherServices iTeacherServices;
+    @RequestMapping({"", "/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("title", "SchoolApp");
+        model.addAttribute("courses", coursesServices.findAll());
+        model.addAttribute("students", getStudents());
+        return "index";
+    }
 
-	@Autowired
-	private ISubjectServices iSubjectServices;
+    @GetMapping("/new-student")
+    public String newStudent(Model model) {
+        model.addAttribute("title", "New Student");
+        model.addAttribute("student", new Student());
+        return "newStudent";
+    }
 
-	private List<Student> getStudents() {
-		return iStudentServices.findAll();
-	}
+    @PostMapping("/save-student")
+    public String save(@Valid @ModelAttribute Student student, BindingResult result, Model model,
+                       RedirectAttributes flash) {
+        if (result.hasErrors()) {
+            model.addAttribute("titulo", "Crear Factura");
+            flash.addFlashAttribute("error", "Complete the form please!!");
+            result.getFieldErrors().forEach(err -> log.error(err.getField()));
+            return "redirect:/new-student";
+        }
+        studentServices.save(student);
+        flash.addFlashAttribute("success", "Factura creada con éxito!");
+        return "redirect:/index";
+    }
 
-	@RequestMapping({ "", "/", "/index" })
-	public String index(Model model) {
-		model.addAttribute("title", "SchoolApp");
-		model.addAttribute("courses", iCourseServices.findAll());
-		model.addAttribute("students", getStudents());
-		return "index";
-	}
+    @RequestMapping({"/students"})
+    public String students(Model model) {
+        model.addAttribute("title", "Students");
+        model.addAttribute("students", studentServices.findAll());
+        return "students";
+    }
 
-	@GetMapping("/new-student")
-	public String newStudent(Model model) {
-		model.addAttribute("title", "New Student");
-		model.addAttribute("student", new Student());
-		return "newStudent";
-	}
+    @RequestMapping({"/student/{id}"})
+    public String studentDetail(Model model, @PathVariable String id) {
+        model.addAttribute("title", "Details");
+        Optional<Student> studentOptional = studentServices.findById(id);
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            model.addAttribute("student", student);
+        } else {
+            model.addAttribute("student", "");
+        }
+        return "studentDetails";
+    }
 
-	@PostMapping("/save-student")
-	public String save(@Valid @ModelAttribute Student student, BindingResult result, Model model,
-			RedirectAttributes flash) {
-		if (result.hasErrors()) {
-			model.addAttribute("titulo", "Crear Factura");
-			flash.addFlashAttribute("error", "Complete the form please!!");
-			result.getFieldErrors().forEach(err -> log.error(err.getField()));
-			return "redirect:/new-student";
-		}
-		iStudentServices.save(student);
-		flash.addFlashAttribute("success", "Factura creada con éxito!");
-		return "redirect:/index";
-	}
+    @GetMapping("/new-course")
+    public String newCourse(Model model) {
+        model.addAttribute("title", "New Course");
+        model.addAttribute("course", new Course());
+        return "newCourse";
+    }
 
-	@RequestMapping({ "/students" })
-	public String students(Model model) {
-		model.addAttribute("title", "Students");
-		model.addAttribute("students", iStudentServices.findAll());
-		return "students";
-	}
+    @GetMapping("/course/{id}")
+    public String courseDetails(@PathVariable String id, Model model) {
+        Optional<Course> courseOptional = coursesServices.findById(id);
+        if (courseOptional.isPresent()) {
+            Course course = courseOptional.get();
+            model.addAttribute("title", "Course " + course.getName());
+            model.addAttribute("course", course);
+            return "courseDetails";
+        }
+        return "redirect:/index";
+    }
 
-	@RequestMapping({ "/student/{id}" })
-	public String studentDetail(Model model, @PathVariable String id) {
-		model.addAttribute("title", "Details");
-		Optional<Student> studentOptional = iStudentServices.findById(id);
-		if (studentOptional.isPresent()) {
-			Student student = studentOptional.get();
-			model.addAttribute("student", student);
-		} else {
-			model.addAttribute("student", "");
-		}
-		return "studentDetails";
-	}
+    @GetMapping("/edit-course/{id}")
+    public String addStudentToCourse(@PathVariable String id, Model model) {
+        Optional<Course> courseOptional = coursesServices.findById(id);
+        if (courseOptional.isPresent()) {
+            Course course = courseOptional.get();
+            model.addAttribute("title", "Course " + course.getName());
+            model.addAttribute("course", course);
+            return "editCourse";
+        }
+        return "redirect:/index";
+    }
 
-	@GetMapping("/new-course")
-	public String newCourse(Model model) {
-		model.addAttribute("title", "New Course");
-		model.addAttribute("course", new Course());
-		return "newCourse";
-	}
+    @PostMapping("/get-student")
+    public String getStudent(@RequestParam String dni, @RequestParam String courseId, RedirectAttributes flash) {
+        Optional<Student> studentOptional = studentServices.findByDni(dni);
+        if (studentOptional.isPresent()) {
+            flash.addFlashAttribute("student", studentOptional.get());
+        }
+        return "redirect:/edit-course/" + courseId;
+    }
 
-	@GetMapping("/course/{id}")
-	public String courseDetails(@PathVariable String id, Model model) {
-		Optional<Course> courseOptional = iCourseServices.findById(id);
-		if (courseOptional.isPresent()) {
-			Course course = courseOptional.get();
-			model.addAttribute("title", "Course " + course.getName());
-			model.addAttribute("course", course);
-			return "courseDetails";
-		}
-		return "redirect:/index";
-	}
-
-	@GetMapping("/edit-course/{id}")
-	public String addStudentToCourse(@PathVariable String id, Model model) {
-		Optional<Course> courseOptional = iCourseServices.findById(id);
-		if (courseOptional.isPresent()) {
-			Course course = courseOptional.get();
-			model.addAttribute("title", "Course " + course.getName());
-			model.addAttribute("course", course);
-			return "editCourse";
-		}
-		return "redirect:/index";
-	}
-
-	@PostMapping("/get-student")
-	public String getStudent(@RequestParam String dni, @RequestParam String courseId, RedirectAttributes flash) {
-		Optional<Student> studentOptional = iStudentServices.findByDni(dni);
-		if (studentOptional.isPresent()) {
-			flash.addFlashAttribute("student", studentOptional.get());
-		}
-		return "redirect:/edit-course/" + courseId;
-	}
-
-	@PostMapping("/add-student")
-	public String addStudent(@RequestParam String courseId, @RequestParam String dni) {
-		Optional<Student> studentOptional = iStudentServices.findByDni(dni);
-		Student student = studentOptional.get();
-		student.setCourse(new Course(courseId));
-		iStudentServices.save(student);
-		return "redirect:/edit-course/" + courseId;
-	}
+    @PostMapping("/add-student")
+    public String addStudent(@RequestParam String courseId, @RequestParam String dni) {
+        Optional<Student> studentOptional = studentServices.findByDni(dni);
+        Student student = studentOptional.get();
+        student.setCourse(Course.builder().id(Long.parseLong(courseId)).build());
+        studentServices.save(student);
+        return "redirect:/edit-course/" + courseId;
+    }
 }
