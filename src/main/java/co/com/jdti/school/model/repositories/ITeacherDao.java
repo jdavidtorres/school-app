955d932d28a02273ded85bf95f3745854eabb4e9
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 16:11:42 -0500

    Update ITeacherDao.java

--- src/main/java/co/com/jdti/school/model/repositories/ITeacherDao.java
+++ src/main/java/co/com/jdti/school/model/repositories/ITeacherDao.java
@@ -1,6 +1,5 @@
 package co.com.jdti.school.model.repositories;
 
-
 import co.com.jdti.school.model.entities.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
