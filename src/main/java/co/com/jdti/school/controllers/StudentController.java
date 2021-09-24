f150a7962e024f8f02f7971f785bf6c65db08bf2
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 15:01:31 -0500

    Update StudentController.java

--- src/main/java/co/com/jdti/school/controllers/StudentController.java
+++ src/main/java/co/com/jdti/school/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package co.com.jdti.school.controllers;
 
-import co.com.jdti.school.services.IStudentServices;
+import co.com.jdti.school.services.StudentServices;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class StudentController {
 
     private final IStudentServices iStudentServices;
