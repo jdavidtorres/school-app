95ed886e0febbb69d50db0ea76c09cab72e81e2f
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 08:49:56 -0500

    Update SchoolApplication.java

--- src/main/java/co/com/jdti/school/SchoolApplication.java
+++ src/main/java/co/com/jdti/school/SchoolApplication.java
@@ -1,15 +1,13 @@
 package co.com.jdti.school;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SchoolApplication implements CommandLineRunner {
+public class SchoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
 }
