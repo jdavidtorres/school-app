84bf75faf2849ec77caf6808f5715856cf2aeb1f
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 17:14:35 -0500

    Update Subject.java

--- src/main/java/co/com/jdti/school/model/entities/Subject.java
+++ src/main/java/co/com/jdti/school/model/entities/Subject.java
@@ -13,7 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
@@ -34,15 +33,7 @@ public class Subject {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Transient
-    private String title;
-
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "subject_id")
     private List<Schedule> schedules;
-
-    public String getTitle() {
-        title = this.name;
-        return title;
-    }
 }
