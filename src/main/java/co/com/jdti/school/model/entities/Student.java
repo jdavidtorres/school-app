dafb7a64208d9c7c6ae5f51b46f28ebef97bdfc4
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-24 15:02:04 -0500

    Update Student.java

--- src/main/java/co/com/jdti/school/model/entities/Student.java
+++ src/main/java/co/com/jdti/school/model/entities/Student.java
@@ -1,5 +1,10 @@
 package co.com.jdti.school.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,15 +14,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
-import org.hibernate.annotations.GenericGenerator;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Entity
 @Table(name = "students")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
