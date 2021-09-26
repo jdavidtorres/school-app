317e092df92b700f2a5abded654f0cd7db775fe3
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-26 14:47:49 -0500

    Update SchoolApplication.java

--- src/main/java/co/com/jdti/school/SchoolApplication.java
+++ src/main/java/co/com/jdti/school/SchoolApplication.java
@@ -33,25 +33,30 @@ public class SchoolApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Teacher teacher2A = teacherServices.save(Teacher.builder().name("Laracrisa Euclidiana").build());
-        Teacher teacher3A = teacherServices.save(Teacher.builder().name("Pepito Perez").build());
+        Teacher director2A = teacherServices.save(Teacher.builder().name("Laracrisa Euclidiana").build());
+        Teacher director3A = teacherServices.save(Teacher.builder().name("Pepito Perez").build());
+        Teacher director4A = teacherServices.save(Teacher.builder().name("Fulanito de Tal").build());
+        Teacher director5A = teacherServices.save(Teacher.builder().name("Juanita Perez").build());
+        Teacher director6A = teacherServices.save(Teacher.builder().name("Pepito Jr. Perez").build());
 
-        Course course1 = coursesServices.save(Course.builder().name("2A").director(teacher2A).build());
-        coursesServices.save(Course.builder().name("3A").director(teacher3A).build());
-        coursesServices.save(Course.builder().name("4A").director(teacherServices.save(Teacher.builder().name("Fulanito de Tal").build())).build());
-        coursesServices.save(Course.builder().name("5A").director(teacherServices.save(Teacher.builder().name("Juanita Perez").build())).build());
-        coursesServices.save(Course.builder().name("6A").director(teacherServices.save(Teacher.builder().name("Pepito Jr. Perez").build())).build());
+        Subject math = subjectServices.save(Subject.builder().name("Matemáticas").build());
+        Subject lang = subjectServices.save(Subject.builder().name("Lenguaje").build());
+        Subject history = subjectServices.save(Subject.builder().name("Historia").build());
+        Subject geometry = subjectServices.save(Subject.builder().name("Geometría").build());
+        Subject ethic = subjectServices.save(Subject.builder().name("Ética").build());
 
-        studentServices.save(Student.builder().dni("dni1").name("Hugo").lastname("Fulano").bloodGroup("A-").address("Calle 1").course(course1).build());
-        studentServices.save(Student.builder().dni("dni2").name("Paco").lastname("Perez").bloodGroup("O-").address("Calle 2").course(course1).fatherName("Pepito").motherName("Perez").build());
-        studentServices.save(Student.builder().dni("dni3").name("Luis").lastname("Mengano").bloodGroup("O+").address("Calle 3").build());
-        studentServices.save(Student.builder().dni("dni4").name("Paco").lastname("Mengano").bloodGroup("O+").address("Calle 4").build());
+        Course course1 = coursesServices.save(Course.builder().name("2A").director(director2A).build());
+        Course course2 = coursesServices.save(Course.builder().name("3A").director(director3A).build());
+        Course course3 = coursesServices.save(Course.builder().name("4A").director(director4A).build());
+        Course course4 = coursesServices.save(Course.builder().name("5A").director(director5A).build());
+        Course course5 = coursesServices.save(Course.builder().name("6A").director(director6A).build());
 
-        Subject subject1 = subjectServices.save(Subject.builder().name("Matemáticas").build());
-        subjectServices.save(Subject.builder().name("Lenguaje").build());
-        subjectServices.save(Subject.builder().name("Historia").build());
-        subjectServices.save(Subject.builder().name("Geometría").build());
-        subjectServices.save(Subject.builder().name("Ética").build());
-        subjectServices.save(Subject.builder().name("Matemáticas").build());
+        Student student1 = studentServices.save(Student.builder().dni("dni1").name("Hugo").lastname("Fulano").bloodGroup("A-").address("Calle 1").courseId(course1.getId()).build());
+        Student student2 = studentServices.save(Student.builder().dni("dni2").name("Paco").lastname("Perez").bloodGroup("O-").address("Calle 2").fatherName("Pepito").motherName("Perez").courseId(course1.getId()).build());
+        Student student3 = studentServices.save(Student.builder().dni("dni3").name("Luis").lastname("Mengano").bloodGroup("O+").address("Calle 3").courseId(course1.getId()).build());
+        Student student4 = studentServices.save(Student.builder().dni("dni4").name("Paco").lastname("Mengano").bloodGroup("O+").address("Calle 4").build());
+
+        Schedule schedule1 = scheduleServices.save(Schedule.builder().start(new Date()).end(new Date()).subjectId(math.getId()).courseId(course1.getId()).title(math.getName()).build());
+        Schedule schedule2 = scheduleServices.save(Schedule.builder().start(new Date()).end(new Date()).subjectId(history.getId()).courseId(course1.getId()).title(history.getName()).build());
     }
 }
