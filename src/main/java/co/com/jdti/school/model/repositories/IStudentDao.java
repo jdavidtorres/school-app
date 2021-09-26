ad6a9bf041d7e97c230971a71f3de2aa3b5765e3
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-26 14:47:03 -0500

    Update IStudentDao.java

--- src/main/java/co/com/jdti/school/model/repositories/IStudentDao.java
+++ src/main/java/co/com/jdti/school/model/repositories/IStudentDao.java
@@ -4,10 +4,13 @@ import co.com.jdti.school.model.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface IStudentDao extends JpaRepository<Student, Long> {
 
     Optional<Student> findByDni(String dni);
+
+    List<Student> findByCourseId(Long courseId);
 }
