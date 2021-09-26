f32c6faecf7a20f9d52c7cfd40d9ce6197c7a78f
Jesus Torres <jesu_ti91@hotmail.com>
2021-09-26 14:47:08 -0500

    Update IScheduleDao.java

--- src/main/java/co/com/jdti/school/model/repositories/IScheduleDao.java
+++ src/main/java/co/com/jdti/school/model/repositories/IScheduleDao.java
@@ -4,7 +4,10 @@ import co.com.jdti.school.model.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IScheduleDao extends JpaRepository<Schedule, Long> {
 
+    List<Schedule> findByCourseId(Long courseId);
 }
