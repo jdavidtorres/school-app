bc66eeae64f5889ccd44316b8f1a4d1c1357719d
Jesus Torres <jesu_ti91@hotmail.com>
2021-10-11 17:21:38 -0500

    Create ScoreReference.java

---
+++ src/main/java/co/com/jdti/school/model/entities/ScoreReference.java
@@ -0,0 +1,35 @@
+package co.com.jdti.school.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "score_references")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoreReference {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Column(name = "subject_id")
+    private Long subjectId;
+}
