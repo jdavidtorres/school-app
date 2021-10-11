c017d3cd8a18312d8a9c811b803e52694cbfac99
Jesus Torres <jesu_ti91@hotmail.com>
2021-10-11 17:21:33 -0500

    Create Score.java

---
+++ src/main/java/co/com/jdti/school/model/entities/Score.java
@@ -0,0 +1,32 @@
+package co.com.jdti.school.model.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "scores")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Score {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", unique = true, nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "reference_id")
+    private Long referenceId;
+
+    @Column(name = "score")
+    private Double score;
+}
